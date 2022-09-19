e024db9c6b333d5fa38e97d6d38ead6675d68f2a
Aaron Ai <yangkun.ayk@gmail.com>
2022-09-19 19:22:31 +0800

    WIP

---
+++ instrumentation/rocketmq-client-java-5.0.1/library/src/main/java/io/opentelemetry/javaagent/instrumentation/rmq/Main.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.javaagent.instrumentation.rmq;
+
+public class Main {
+  private Main() {}
+
+  public static void main(String[] args) {}
+}
