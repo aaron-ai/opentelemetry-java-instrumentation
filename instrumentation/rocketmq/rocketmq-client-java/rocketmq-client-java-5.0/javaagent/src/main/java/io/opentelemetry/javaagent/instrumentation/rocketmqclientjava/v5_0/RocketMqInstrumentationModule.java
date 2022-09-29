faec9bd23a4d1909f3b8fbaade51b528beb222a4
Aaron Ai <yangkun.ayk@gmail.com>
2022-09-29 14:53:50 +0800

    WIP

--- instrumentation/rocketmq/rocketmq-client-java/rocketmq-client-java-5.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/rocketmqclientjava/v5_0/RocketMqInstrumentationModule.java
+++ instrumentation/rocketmq/rocketmq-client-java/rocketmq-client-java-5.0/javaagent/src/main/java/io/opentelemetry/javaagent/instrumentation/rocketmqclientjava/v5_0/RocketMqInstrumentationModule.java
@@ -5,14 +5,16 @@
 
 package io.opentelemetry.javaagent.instrumentation.rocketmqclientjava.v5_0;
 
+import com.google.auto.service.AutoService;
 import io.opentelemetry.javaagent.extension.instrumentation.InstrumentationModule;
 import io.opentelemetry.javaagent.extension.instrumentation.TypeInstrumentation;
 import java.util.Collections;
 import java.util.List;
 
+@AutoService(InstrumentationModule.class)
 public class RocketMqInstrumentationModule extends InstrumentationModule {
   public RocketMqInstrumentationModule() {
-    super("rocketmq-client", "rocketmq-client-java-5.0");
+    super("rocketmq-client-java", "rocketmq-client-java-5.0");
   }
 
   @Override
